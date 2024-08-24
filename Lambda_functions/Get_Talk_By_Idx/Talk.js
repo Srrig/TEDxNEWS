c6a6c9adf3b496813af6c9b5518daac16c769ce9
RuggeriSiria <134622305+Srrig@users.noreply.github.com>
2024-08-24 18:50:00 +0200

    Update Talk.js

--- Lambda_functions/Get_Talk_By_Idx/Talk.js
+++ Lambda_functions/Get_Talk_By_Idx/Talk.js
@@ -8,9 +8,9 @@ const talkSchema = new mongoose.Schema({
     title: String,         // Titolo del talk
     url: String,           // URL del talk
     description: String,   // Descrizione del talk
+    image_url: String,     // URL dell'immagine
     duration: String,      // Durata del talk
-    num_views: { type: Number, default: 0 }, // Numero di visualizzazioni     // Numero di visualizzazioni
-    watch_next_s: [String] // Array di ID dei video suggeriti (watch next)
+    tags: [String],        // Array di tag associati al talk
 }, { collection: 'tedx_data' });
 
 // Creazione e esportazione del modello
