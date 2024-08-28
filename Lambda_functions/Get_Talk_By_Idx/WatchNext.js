2d58b9534a544095d618a8b254f28cdf0c54dfd7
RuggeriSiria <134622305+Srrig@users.noreply.github.com>
2024-08-28 16:05:25 +0200

    Update WatchNext.js

--- Lambda_functions/Get_Talk_By_Idx/WatchNext.js
+++ Lambda_functions/Get_Talk_By_Idx/WatchNext.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const watchNextSchema = new mongoose.Schema({
     video_id: String,                      // ID del video principale
-    related_videos_id_list: [String],      // Lista di ID dei video correlati
-    tags: [String]                         // Array di tag associati al video
+    related_videos_id_list: [String],// Lista di ID dei video correlati
+    image_url: String,
+    url: String,
+    tags: [String],                         // Array di tag associati al video
 }, { collection: 'watch_next' });
 
 module.exports = mongoose.model('WatchNext', watchNextSchema);
