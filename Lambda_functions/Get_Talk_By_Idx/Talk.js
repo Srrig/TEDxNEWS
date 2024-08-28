6d231960a2d27a599b609aa82a675170f6f2665a
RuggeriSiria <134622305+Srrig@users.noreply.github.com>
2024-08-28 16:05:49 +0200

    Update Talk.js

--- Lambda_functions/Get_Talk_By_Idx/Talk.js
+++ Lambda_functions/Get_Talk_By_Idx/Talk.js
@@ -6,10 +6,10 @@ const talkSchema = new mongoose.Schema({
     slug: String,          // Slug del talk
     speakers: String,      // Relatori del talk
     title: String,         // Titolo del talk
-    url: String,           // URL del talk
+    url: String,            // URL del talk
     description: String,   // Descrizione del talk
-    image_url: String,     // URL dell'immagine
     duration: String,      // Durata del talk
+    image_url: String,      //url immagine
     tags: [String],        // Array di tag associati al talk
 }, { collection: 'tedx_data' });
 
